f9767e7cff987c41cefddb6c790322f7e8877d30
benkivuva <benkivuvak5@gmail.com>
2023-02-16 06:51:03 -0800

    one more

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - entry point
  *
- * Description: Prints a specific message using the function puts
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	/* Prints the specified message */
-	puts("Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
