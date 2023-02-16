a465f9d735ec6744d3527323f43c5f77f77d0c23
benkivuva <benkivuvak5@gmail.com>
2023-02-16 06:43:34 -0800

    updating betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message using the function puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	puts("Programming is like building a multilingual puzzle");
-	return 0;
+{
+	/* Prints the specified message */
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
 }
