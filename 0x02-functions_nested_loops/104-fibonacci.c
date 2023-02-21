dc53dbf9831501a6c37723b01e0dffeb25cba9d5
benkivuva <benkivuvak5@gmail.com>
2023-02-21 08:55:05 -0800

    updating

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,41 +1,63 @@
 #include <stdio.h>
 
+
 /**
- * main - Entry point
- * Description: prints first 98 Fibonacci numbers
- * Return: Always 0 (Success)
+ * numLength - returns the lenth of string
+ * @num : operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+/**
+ *  *main - prints the first 98 fibonaci sequences
+ *  Return: 0
  */
 
 int main(void)
 {
-	int i;
-	unsigned long int fib1 = 1, fib2 = 2, sum = 0;
-	unsigned long int fib1_prev = 0, fib2_prev = 1;
-	unsigned long int carry, fib1_new, fib2_new;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int i = 1, initial0s;
 
-	printf("%lu, %lu", fib1, fib2);
-
-	for (i = 2; i < 98; i++)
+	while (i <= 98)
 	{
-	if (sum > 0)
-		printf(", ");
-	carry = 0;
-	fib1_new = fib1_prev + fib1;
-	fib2_new = fib2_prev + fib2;
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
 
-	if (fib2_new > 1000000000000000000)
-	{
-		carry = fib2_new / 1000000000000000000;
-		fib2_new %= 1000000000000000000;
+		tmp = (f1 + f2) % mx;
+		tmpo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
 	}
-		sum = carry + fib1_new + fib2_new;
-		printf("%lu", sum);
-		fib1_prev = fib1;
-		fib2_prev = fib2;
-		fib1 = fib1_new;
-		fib2 = fib2_new;
-	}
-	printf("\n")
-	return (0)
+	return (0);
 }
-
