be176dce6af12975145606b8462489a7bf834fac
benkivuva <benkivuvak5@gmail.com>
2023-02-21 03:33:21 -0800

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 1; i < 8; i++)
-		_putchar(word[8]);
-	_putchar ('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+	}
