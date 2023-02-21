52457d1232330347e14f8401bcfe453c50a31892
benkivuva <benkivuvak5@gmail.com>
2023-02-21 03:55:50 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower = < 'z'; a++)
+	_putchar(lower);
+	_putchar("\n");
+}
