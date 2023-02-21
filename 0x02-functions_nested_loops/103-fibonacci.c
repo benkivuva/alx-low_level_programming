bb0864e0016889507b8327e2a07926a5d5a7a1e0
benkivuva <benkivuvak5@gmail.com>
2023-02-21 08:05:04 -0800

    updated

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci numbers whose values do not exceed 4,000,000
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int sum = 0, current = 1, prev = 0, temp;
+
+	while (current <= 4000000)
+	{
+		temp = current;
+		current += prev;
+		prev = temp;
+		if (current % 2 == 0)
+			sum += current;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
