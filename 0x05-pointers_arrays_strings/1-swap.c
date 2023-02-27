8fc5afa62273f7a7cdaa522289dc7ed985f80b68
benkivuva <benkivuvak5@gmail.com>
2023-02-27 04:30:05 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: a pointer to the first integer
+ * @b: a pointer to the second integer
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
