52d2e3a17b953cdbe4d57408d8ca58dde1583231
benkivuva <benkivuvak5@gmail.com>
2023-02-27 04:36:14 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer to the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
