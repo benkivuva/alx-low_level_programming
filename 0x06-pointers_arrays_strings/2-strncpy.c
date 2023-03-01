2abb4fd21abca81a2ca258e6004697e8eb1984a2
benkivuva <benkivuvak5@gmail.com>
2023-02-28 23:41:17 -0800

    corected task 0 and 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,12 @@
 /**
- * _strcat - Concatenates two strings
+ * _strncat - Concatenates two strings up to n bytes
  * @dest: Pointer to destination string
  * @src: Pointer to source string
+ * @n: Maximum number of bytes to concatenate from src
  *
  * Return: Pointer to concatenated string
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
@@ -18,7 +19,7 @@ char *_strcat(char *dest, char *src)
 
 	/* Append the source string to the destination string */
 	j = 0;
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
