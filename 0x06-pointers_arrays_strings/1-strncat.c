199f7a3335b74dc4dfc2a0a54f5eb516257c6e69
benkivuva <benkivuvak5@gmail.com>
2023-02-28 23:25:20 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ *
+ * Return: Pointer to concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	/* Find the end of the destination string */
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Append the source string to the destination string */
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add a null terminator to the end of the concatenated string */
+	dest[i] = '\0';
+
+	/* Return a pointer to the concatenated string */
+	return dest;
+}
