227025a73bd26d2d23c45ab7a33b0acd66477cf7
benkivuva <benkivuvak5@gmail.com>
2023-03-08 04:05:24 -0800

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the base number
+ * @y: the exponent
+ *
+ * Return: x raised to the power of y, or -1 if y is negative
+ */
+int _pow_recursion(int x, int y)
+{
+	/* Base case: if y is less than 0, return -1 to indicate an error */
+	if (y < 0)
+	{
+		return (-1);
+	}
+	/* Base case: if y is 0, x^0 is 1 */
+	else if (y == 0)
+	{
+		return (1);
+	}
+	/* Recursive case: return x times x^(y-1) */
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
