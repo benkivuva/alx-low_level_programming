178dbf0f2b0576e0c98f361339431e115a8668f3
benkivuva <benkivuvak5@gmail.com>
2023-03-08 04:45:55 -0800

    advanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,11 +7,11 @@
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
-    if (s[start] != s[end])
-        return 0;
-    return is_palindrome_helper(s, start + 1, end - 1);
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
@@ -21,7 +21,7 @@ int is_palindrome_helper(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = strlen(s);
-    return is_palindrome_helper(s, 0, len - 1);
-}
+	int len = strlen(s);
 
+	return (is_palindrome_helper(s, 0, len - 1));
+}
