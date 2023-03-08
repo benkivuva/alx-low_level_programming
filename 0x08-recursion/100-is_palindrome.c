d44f67bb6b04b8e6964d12ea4e0c5aa79d4bc3ab
benkivuva <benkivuvak5@gmail.com>
2023-03-08 04:53:48 -0800

    advanced 1 corrected

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,4 @@
 /**
- * is_palindrome_helper - helper function for is_palindrome
- * @s: the string to check for palindromicity
- * @start: the starting index of the string
- * @end: the ending index of the string
- * Return: 1 if s is a palindrome, 0 otherwise
- */
-int is_palindrome_helper(char *s, int start, int end)
-{
-	if (start >= end)
-		return (1);
-	if (s[start] != s[end])
-		return (0);
-	return (is_palindrome_helper(s, start + 1, end - 1));
-}
-
-/**
  * is_palindrome - checks if a string is a palindrome
  * @s: the string to check
  * Return: 1 if s is a palindrome, 0 otherwise
@@ -23,5 +7,10 @@ int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-	return (is_palindrome_helper(s, 0, len - 1));
+	if (len <= 1)
+		return (1);
+	if (*s != *(s + len - 1))
+		return (0);
+	*(s + len - 1) = '\0';
+	return (is_palindrome(s + 1));
 }
