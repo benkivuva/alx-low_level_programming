33490aa918bf3afdc2d9c20e101f6e6f7f1877cc
benkivuva <benkivuvak5@gmail.com>
2023-03-16 07:19:58 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: Number of elements to allocate memory for
+ * @size: Size of each element in bytes
+ *
+ * Return: Pointer to the allocated memory, or NULL if allocation fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	void *ptr = malloc(nmemb * size);
+
+	if (ptr != NULL)
+	{
+		char *p = ptr;
+
+		for (unsigned int i = 0; i < nmemb * size; i++)
+			*(p + i) = 0;
+	}
+
+	return (ptr);
+}
