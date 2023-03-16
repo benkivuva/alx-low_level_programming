2f7b426df0cd2163b4fe26e75e467d0b85095ed9
benkivuva <benkivuvak5@gmail.com>
2023-03-16 07:54:50 -0700

    task updated 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,16 +9,24 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
+	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	void *ptr = malloc(nmemb * size);
-
-	if (ptr != NULL)
 	{
-		char *p = ptr;
-		for (unsigned int i = 0; i < nmemb * size; i++)
-			*(p + i) = 0;
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char *)ptr + i) = 0;
 	}
 
 	return (ptr);
