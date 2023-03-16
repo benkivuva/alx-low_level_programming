ff73a87961e7503a62f55edffa1a43c5635f6374
benkivuva <benkivuvak5@gmail.com>
2023-03-16 07:40:50 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array
+ * @nmemb: Number of elements to allocate memory for
+ * @size: Size of each element in bytes
+ *
+ * Return: Pointer to the allocated memory, or NULL if allocation fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NUll);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		*((char *)ptr + i) = 0;
+	}
+
+	return (ptr);
+}
