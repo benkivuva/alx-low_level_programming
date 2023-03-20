e8b37ee5bd6b70ee2e12075c21b9f21368506f42
benkivuva <benkivuvak5@gmail.com>
2023-03-20 09:58:46 -0700

    hot fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+		{
+			return (NULL);
+		}
+
 		return (new_ptr);
 	}
 
