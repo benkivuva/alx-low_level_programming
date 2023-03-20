699fbe3bf2983f9c9e15f97f373bc02430dbafd7
benkivuva <benkivuvak5@gmail.com>
2023-03-20 07:24:55 -0700

    updated task o

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - short description
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
