41d893d20d2f4d111dfd7c3b88662858f871352c
benkivuva <benkivuvak5@gmail.com>
2023-03-22 08:12:24 -0700

    corrected opcode

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,34 +17,38 @@ void print_opcodes(int n_bytes);
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    int n_bytes = atoi(argv[1]);
+	int n_bytes = atoi(argv[1]);
 
-    if (n_bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
+	if (n_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 
-    print_opcodes(n_bytes);
+	print_opcodes(n_bytes);
 
-    return (0);
+	return (0);
 }
 
+/**
+ * print_opcodes - prints the opcodes of the main function
+ * @n_bytes: the number of bytes of opcodes to print
+ */
 void print_opcodes(int n_bytes)
 {
-    void (*main_ptr)(void) = (void (*)(void))&main;
-    int i;
+	void (*main_ptr)(void) = (void (*)(void))&main;
+	int i;
 
-    for (i = 0; i < n_bytes; i++)
-    {
-        printf("%02x ", *((unsigned char *)main_ptr + i));
-    }
+	for (i = 0; i < n_bytes; i++)
+	{
+		printf("%02x ", *((unsigned char *)main_ptr + i));
+	}
 
-    printf("\n");
+	printf("\n");
 }
