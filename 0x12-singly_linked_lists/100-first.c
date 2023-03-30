e8a0d2beaca2162616af71615445d635721b4e48
Benson Kivuva <benkivuvak5@gmail.com>
2023-03-30 08:25:39 -0700

    last task corrected

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-/**
- * pre_main - prints a message before main is executed
- */
-void pre_main(void)
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
+void __attribute__((constructor)) hare(void);
 
 /**
- * main - entry point for the program
- *
- * Return: Always 0
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-int main(void)
+void hare(void)
 {
-	pre_main();
-
-	return (0);
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
