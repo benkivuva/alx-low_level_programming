5e23cfd8a09a6a5df047972f849bc92aba1e30b4
Benson Kivuva <benkivuvak5@gmail.com>
2023-03-30 07:56:46 -0700

    task 4 done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * free_list - Frees a linked list
+ * @head: Pointer to the head node of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
