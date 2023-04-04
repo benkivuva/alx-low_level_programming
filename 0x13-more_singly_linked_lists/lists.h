13c859ada82a3d7d6f8b78084f4e1f3712a103f3
Benson Kivuva <benkivuvak5@gmail.com>
2023-04-04 05:20:42 -0700

    task 6

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,5 +27,6 @@ void free_listint2(listint_t **head);
 size_t listint_len(const listint_t *h);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
+int pop_listint(listint_t **head);
 
 #endif /* LISTS_H */
