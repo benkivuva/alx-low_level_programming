55b81c567a3bbabb8f1ac1cf1432a8483ddd755e
Benson Kivuva <benkivuvak5@gmail.com>
2023-04-04 06:49:21 -0700

    advance tasks

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the first node of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+    const listint_t *slow = head, *fast = head;
+    size_t count = 0;
+
+    while (slow && fast && fast->next)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        count++;
+
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            printf("[%p] %d\n", (void *)slow, slow->n);
+            count++;
+
+            slow = slow->next;
+            while (slow != fast)
+            {
+                printf("[%p] %d\n", (void *)slow, slow->n);
+                count++;
+                slow = slow->next;
+            }
+            printf("Loop starts at [%p] %d\n", (void *)slow, slow->n);
+            return count;
+        }
+    }
+
+    while (slow)
+    {
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        count++;
+        slow = slow->next;
+    }
+
+    return count;
+}
