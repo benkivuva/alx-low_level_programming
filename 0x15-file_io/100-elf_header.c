23b1daf2467d397c0ceae4ae281a27ab95447cd7
Benson Kivuva <benkivuvak5@gmail.com>
2023-04-10 03:03:56 -0700

    task 4.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,12 @@
-#include "main.h"
+#include <main.h>
+#include <fcntl.h>
 #include <elf.h>
+
+void print_magic(unsigned char *magic);
+void print_class(unsigned char class);
+void print_data(unsigned char data);
+void print_version(unsigned char version);
+void print_osabi(unsigned char osabi);
 
 /**
  * main - displays the ELF header of an ELF file
@@ -48,32 +55,49 @@ int main(int argc, char *argv[])
     }
 
     /* Print the ELF header information */
-    printf("Magic:   %02x %02x %02x %02x\n",
-           header.e_ident[EI_MAG0],
-           header.e_ident[EI_MAG1],
-           header.e_ident[EI_MAG2],
-           header.e_ident[EI_MAG3]);
+    print_magic(header.e_ident);
+    print_class(header.e_ident[EI_CLASS]);
+    print_data(header.e_ident[EI_DATA]);
+    print_version(header.e_ident[EI_VERSION]);
+    print_osabi(header.e_ident[EI_OSABI]);
 
+    printf("ABI Version:                       %d\n",
+           header.e_ident[EI_ABIVERSION]);
+
+    printf("Type:                              %s\n",
+           header.e_type == ET_NONE ? "NONE (None)" :
+           header.e_type == ET_REL ? "REL (Relocatable file)" :
+           header.e_type == ET_EXEC ? "EXEC (Executable file)" :
+           header.e_type == ET_DYN ? "DYN (Shared object file)" :
+           header.e_type == ET_CORE ? "CORE (Core file)" :
+           "unknown");
+
+    printf("Entry point address:               0x%lx\n",
+           (unsigned long) header.e_entry);
+
+    close(fd);
+    return (0);
+}
+
+/**
+ * print_magic - prints the ELF magic number
+ * @magic: a pointer to the magic number bytes
+ */
+void print_magic(unsigned char *magic)
+{
+    printf("Magic:   ");
+    for (int i = 0; i < EI_NIDENT; i++)
+        printf("%02x ", magic[i]);
+    printf("\n");
+}
+
+/**
+ * print_class - prints the ELF class
+ * @class: the class byte from the ELF header
+ */
+void print_class(unsigned char class)
+{
     printf("Class:                             %s\n",
-           header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
-           header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
-           header.e_ident[EI_CLASS] == ELFCLASSNONE ? "none" : "unknown");
-
-    printf("Data:                              %s\n",
-           header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-           header.e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" :
-           header.e_ident[EI_DATA] == ELFDATANONE ? "none" : "unknown");
-
-    printf("Version:                           %d\n",
-           header.e_ident[EI_VERSION]);
-
-    printf("OS/ABI:                            %s\n",
-           header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
-           header.e_ident[EI_OSABI] == ELFOSABI_HPUX ? "UNIX - HP-UX" :
-           header.e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
-           header.e_ident[EI_OSABI] == ELFOSABI_LINUX ? "UNIX - Linux" :
-           header.e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
-           header.e_ident[EI_OSABI] == ELFOSABI_AIX ? "UNIX - AIX" :
-           header.e_ident[EI_OSABI] == ELFOSABI_IRIX ? "UNIX - IRIX" :
-           header.e_ident[EI_OSABI] == ELFOSABI_FREEBSD ? "
-
+           class == ELFCLASS64 ? "ELF64" :
+           class == ELFCLASS32 ? "ELF32" :
+           class == ELFCLASSNONE ? "none" : "unknown");
