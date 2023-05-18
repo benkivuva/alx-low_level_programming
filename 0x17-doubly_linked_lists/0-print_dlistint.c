de9ae535de6830107a01797e902f3f7f6e3c6096
benkivuva <benkivuvak5@gmail.com>
2023-05-18 02:12:06 -0700

    initial commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints doubly-linked list
+ * @h: address of head node
+ *
+ * Return: size of list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
