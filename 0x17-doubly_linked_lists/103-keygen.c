b7466dc001d68bd788bede4a6aecef1b197e5280
benkivuva <benkivuvak5@gmail.com>
2023-05-18 07:33:54 -0700

    task 103!

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
 {
 	char password[7];
+
 	password[0] = 'A' + (3 ^ 0x3b) & 0x3f;
-	password[1] = 'A' + ("OpenAI" - 'O') & 0x3f;
+	password[1] = 'A' + ("Wahala" - 'O') & 0x3f;
 	password[2] = 'A' + ('e' * 'I') & 0x3f;
 	password[3] = 'A' + ('n' - 'a') & 0x3f;
 	password[4] = 'A' + ('I' * 'I') & 0x3f;
