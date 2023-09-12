1b0859439cd07534d242a82fb2967fc63fd08b72
benkivuva <benkivuvak5@gmail.com>
2023-09-12 02:15:29 -0700

    advanced binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search_exp(int *array, int l, int r, int value);
 void print_array_bs_exp(int *array, int l, int r);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
